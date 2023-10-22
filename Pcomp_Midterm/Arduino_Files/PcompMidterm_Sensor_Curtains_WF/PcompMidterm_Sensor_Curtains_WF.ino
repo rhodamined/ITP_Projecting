fd89965623f64b490cff2617c39328b1b2f4d113
rhodamined <rhodamined@gmail.com>
2023-10-22 15:37:38 -0400

    curtain shakes, opens, does not close

--- Pcomp_Midterm/Arduino_Files/PcompMidterm_Sensor_Curtains_WF/PcompMidterm_Sensor_Curtains_WF.ino
+++ Pcomp_Midterm/Arduino_Files/PcompMidterm_Sensor_Curtains_WF/PcompMidterm_Sensor_Curtains_WF.ino
@@ -1,11 +1,31 @@
 /* PComp - Fall 2023 - Midterm Project */
 /* Mingxuan Li & Christina Tang */
 
-/* Testing.... */
-const int LEDPin = 13;
+/* TESTING */
+const int LEDPin = 13;    // used for testing
+const int RedLEDPin = 10;  // used for calibrating
+const int buttonPinMode = 9;  // blue button for calibrating
+
+
+const int testing = 0; // testing mode: actually open or close curtains
+// 0: testing OFF, run as normal
+// 1: testing ON, do not open curtains, only give sensor data
+
+// 0: CALIBRATING: use pushbuttons for calibration of motors, sensor OFF
+// 1: RUN: sensor ON, all things behave as expected
+int runMode = 0;        // always start in CALIBRATING
+int lastRunButtonState = LOW;  // the previous reading from the input pin
+int timeA = 0;
+int timeB = 0;
+
+// the following variables are unsigned longs because the time, measured in
+// milliseconds, will quickly become a bigger number than can be stored in an int.
+unsigned long lastRunButtonDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long runButtonDebounceDelay = 50;    // the debounce time; increase if the output flickers
+
 
 /* MOTOR A - CURTAIN OPEN & CLOSE */
-const int buttonPinUp = 2;    // pushbutton - to go UP
+const int buttonPinUp = 2;      // pushbutton - to go UP
 const int buttonPinDown = 6;    // pushbutton - to go DOWN
 const int buttonPinSolenoid = 7;
 
@@ -14,7 +34,7 @@ const int motorA2Pin = 4;   // Motor Driver - Motor A Leg 2
 const int pwmAPin = 5;      // Motor Driver - PWM Motor A
 
 /* SOLENOID - for 'shaking' the curtain */
-const int solenoidPin = 8;  // Solenoid - the tugging?
+const int solenoidPin = 8;  // Solenoid 
 
 /* DISTANCE SENSOR */
 const int EtrigPin = 18;
@@ -29,12 +49,14 @@ unsigned long debounceDelay = 1000;   // length of time someone is within distan
 float hingeDistance = 50;             // The distance at which things change: 50cm
 
 
-/* TESTING */
-int testing = 0;  // 0: run as normal 1: manual control only, no sensor input
 
 void setup() {
   // LED for testing
   pinMode(LEDPin, OUTPUT);
+  pinMode(RedLEDPin, OUTPUT);
+  
+  // pushbutton switch for mode
+  pinMode(buttonPinMode, INPUT);
 
   // pushbutton switch as input
   pinMode(buttonPinUp, INPUT);
@@ -63,115 +85,182 @@ void setup() {
 
 void loop() {
 
-  // 2 buttons for LIFT UP curtain and LET DOWN curtain
-  // lift up -- green button
-  if (digitalRead(buttonPinUp) == HIGH){
-    openCurtain();
+
+  // TOGGLE calibration / run mode with button
+  int modePinReading = digitalRead(buttonPinMode);
+
+  if (modePinReading == HIGH && lastRunButtonState == LOW) {
+    timeA = millis();
+    lastRunButtonState = HIGH;
   }
-  // let down -- red button
-  else if (digitalRead(buttonPinDown) == HIGH){
-    closeCurtain();
+  if (modePinReading == LOW && lastRunButtonState == HIGH) {
+    timeB = millis();
+    lastRunButtonState = LOW;
+    if (timeB - timeA > runButtonDebounceDelay) {
+      if (runMode == 0) {
+        runMode = 1;
+      } else if (runMode == 1) {
+        runMode = 0;
+      }
+    }
+  }
+
+  // Red LED indicator light: ON is CALIBRATING, OFF is RUNNING
+  if (runMode == 0) {
+    digitalWrite(RedLEDPin, HIGH);
+  } else if (runMode == 1){
+    digitalWrite(RedLEDPin, LOW);
   } 
-  // when not pressing either -- do nothing
-  else {
-    digitalWrite(LEDPin, LOW);
-    digitalWrite(pwmAPin, LOW);
+
+  /* CALIBRATION */
+  if (runMode == 0) { 
+        
+    // 2 buttons for debugging - hold down button to turn motor
+    // lift up / open curtain -- green button
+    if (digitalRead(buttonPinUp) == HIGH){
+      ASpinCW();
+    }
+    // let down -- red button
+    else if (digitalRead(buttonPinDown) == HIGH){
+      ASpinCCW();
+    } 
+    // when not pressing either -- do nothing
+    else {
+      digitalWrite(LEDPin, LOW);  // indicator light
+      stopMotor();                // stop 
+    }
+
+    // press button to fire solenoid
+    if (digitalRead(buttonPinSolenoid) == HIGH){
+      solenoidStrikeSpeed(50);
+    }
+
   }
-
-  // press button to fire solenoid
-  if (digitalRead(buttonPinSolenoid) == HIGH){
-    Serial.println("solenoid");
-    solenoidStrike();
-  }
-
-  // distance sensor
-  digitalWrite(EtrigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(EtrigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(EtrigPin, LOW);
-
-
-  Eduration = pulseIn(EechoPin, HIGH);
-  Edistance = (Eduration / 2) * 0.0343;
-
-  // Serial monitoring: distance sensed
-  Serial.print("EDistance = ");
-  Serial.print(Edistance);
-  Serial.println(" cm");
-  delay(200);
   
+  else if (runMode == 1) {
+    Serial.println("mode is RUNNING");
+  
+    // distance sensor
+    digitalWrite(EtrigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(EtrigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(EtrigPin, LOW);
 
-  //
-  if (Edistance < hingeDistance) {
-    Serial.println("Hdistance close enough");
-  } 
+    Eduration = pulseIn(EechoPin, HIGH);
+    Edistance = (Eduration / 2) * 0.0343;
 
-  /* "DEBOUNCE" */
-  // borrowed & adapted from: https://docs.arduino.cc/built-in-examples/digital/Debounce
+    // Serial monitoring: distance sensed
+    Serial.print("EDistance = ");
+    Serial.print(Edistance);
+    Serial.println(" cm");
+  
+      
+    // monitoring
+    if (Edistance < hingeDistance) {
+      Serial.println("Edistance close enough");
+    } 
 
-  // close enough and has previously been far
-  if (Edistance < hingeDistance && lastState == 1) {   
-    // reset the debouncing timer
-    lastDebounceTime = millis();
-  }   
-  else if (Edistance > hingeDistance && lastState == 0) { 
-    // reset the debouncing timer
-    lastDebounceTime = millis();
-  }
+    // if distance is greater than 1m and curtain is CLOSED - shake slowly, but faster as ppl approach
+    if (Edistance > 100 && lastState == 0) {
+      int spd = map(Edistance, 50, 400, 50, 3000);
+      Serial.println(spd);
+      solenoidStrikeSpeed(spd);
+    
+    // if distance is between 50cm-1m and curtain is CLOSED - shake fast!
+    } else if (Edistance >= 50 && Edistance <= 100 && lastState == 0) {
+      Serial.println("50-100");
+      solenoidStrike();
+    } else if (Edistance < 50) {
 
-  if ((millis() - lastDebounceTime) > debounceDelay) {
-    // whatever the reading is at, it's been there for longer than the debounce
-    // delay, so take it as the actual current state:
+      // borrowed & adapted from: https://docs.arduino.cc/built-in-examples/digital/Debounce
+      // close enough and has previously been far
+      if (Edistance < 50 && lastState == 1) { // OPEN
+        lastDebounceTime = millis();      // reset the debounce timer
+        
 
-    if (lastState == 0){    // move from FAR to NEAR
-      Serial.println("OPEN");
-      openCurtain();
-      delay(200);
-      lastState = 1;
+      }   
+      else if (Edistance > 50 && lastState == 0) { // CLOSE
+        lastDebounceTime = millis();      // reset the debounce timer
+      }
+
+      if ((millis() - lastDebounceTime) > debounceDelay) {
+        // whatever the reading is at, it's been there for longer than the debounce
+        // delay, so take it as the actual current state:
+
+        if (lastState == 0){          // move from FAR to NEAR: OPEN curtain
+          Serial.println("OPEN");
+          if (testing == 0) {         // not testing mode: open curtain
+            openCurtain();
+          }
+          delay(200);
+          lastState = 1;              // set state to OPEN
+        }
+        else if (lastState == 1){     // move from NEAR to FAR: CLOSE curtain
+          Serial.println("CLOSE");
+          if (testing == 0){
+            closeCurtain();
+          }
+          delay(200);
+          lastState = 0;              // set state to CLOSED
+        }
+      }
     }
-    else if (lastState == 1){    // move from NEAR to FAR
-      Serial.println("CLOSE");
-      closeCurtain();
-      delay(200);
-      lastState = 0;
-    }
+
+    
+    
   }
-
-
 }
 
-
+// spin motor A clockwise (CW) when viewed from behind (programming view lol)
 void ASpinCW() {
   digitalWrite(pwmAPin, HIGH);
   digitalWrite(motorA1Pin, HIGH);
   digitalWrite(motorA2Pin, LOW);
 }
 
+// spin motor A counterclockwise (CCW) when viewed from behind (programming view lol)
 void ASpinCCW() {
   digitalWrite(pwmAPin, HIGH);
   digitalWrite(motorA1Pin, LOW);
   digitalWrite(motorA2Pin, HIGH);
 }
 
-void solenoidStrike() {
-  digitalWrite(solenoidPin, HIGH);
-  delay(100);
-  digitalWrite(solenoidPin, LOW);
+// do not spin motor
+void stopMotor() {
+  digitalWrite(pwmAPin, LOW); // stop
 }
 
+// pulse solenoid at constant rate
+void solenoidStrike() {
+  digitalWrite(solenoidPin, HIGH);
+  delay(50);
+  digitalWrite(solenoidPin, LOW);
+  delay(50);
+}
+
+// pulse solenoid with [arg] rate -- rate is in milliseconds
+void solenoidStrikeSpeed(int speed) {
+  digitalWrite(solenoidPin, HIGH);
+  delay(50);
+  digitalWrite(solenoidPin, LOW);
+  delay(speed);
+}
+
+// open curtain: spin 'CW' for hard-coded value of time in ms
 void openCurtain() {
   digitalWrite(LEDPin, HIGH);
   Serial.println("yay UP");
   ASpinCW();
-  delay(900);                 // run motor for this amt of time
-  digitalWrite(pwmAPin, LOW); // stop
+  delay(800);                 // run motor for this amt of time
+  stopMotor();                // stop
 }
 
+// close curtain: spin 'CCW' for had-coded value of time in ms
 void closeCurtain() {
   digitalWrite(LEDPin, HIGH);
   Serial.println("yay DOWN");
   ASpinCCW();
-  delay(900);                 // run motor for this amt of time
-  digitalWrite(pwmAPin, LOW); // stop
+  delay(800);                 // run motor for this amt of time
+  stopMotor();                // stop
 }
