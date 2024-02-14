034d58c4ed34ba7fa784ec16a173280d3902c8e9
rhodamined <rhodamined@gmail.com>
2024-02-14 02:46:47 -0500

    height attached to arduino data

--- Connected_Devices_SP24/HW3/public/script.js
+++ Connected_Devices_SP24/HW3/public/script.js
@@ -4,15 +4,43 @@ const socketURL = `${config.protocol}://${config.domain}:${(config.port).toStrin
 
 const socket = new WebSocket(socketURL);
 
-socket.addEventListener("open", handleSocketOpen);
+// socket.addEventListener("open", handleSocketOpen);
 
-function handleSocketOpen() {
-    document.body.style.backgroundColor = "green";
-}
+// function handleSocketOpen() {
+//     document.body.style.backgroundColor = "green";
+// }
 
 socket.addEventListener("message", handleSocketMessage);
 
+
+const sensorMin = 30;
+const sensorMax = 450;
+
 function handleSocketMessage(event) {
-    console.log("socket");
-    document.getElementById("socket-data").innerHTML = event.data;
+    // console.log("socketMessageEvent");
+    
+    let num = parseInt(event.data);
+    console.log(num);
+
+    let y = 0;
+
+    let elem = document.getElementById("socket-data");
+
+    if (num >= sensorMin && num < sensorMax) {
+        y = map_range(num, sensorMin, sensorMax, 0, 200);
+    } else if (num < sensorMin) {
+        y = 0;
+    } else if (num > sensorMax) {
+        // y = -window.innerHeight;
+    }
+    console.log(y);
+
+    elem.innerHTML = event.data;
+    elem.style.height = y+"px";
+}
+
+// always too lazy to write it myself
+// https://stackoverflow.com/questions/5649803/remap-or-map-function-in-javascript
+function map_range(value, low1, high1, low2, high2) {
+    return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
 }
