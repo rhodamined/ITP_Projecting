bdf41fa166354bd2cf0063a2b1e6817b899f37a5
rhodamined <rhodamined@gmail.com>
2024-02-20 20:33:50 -0500

    working MQTT

--- Connected_Devices_SP24/HW4/MQTT_Sender/MQTT_Sender.ino
+++ Connected_Devices_SP24/HW4/MQTT_Sender/MQTT_Sender.ino
@@ -1,36 +1,77 @@
+//------ INCLUDE ------//
 #include <ArduinoMqttClient.h>
+#include <Adafruit_VL53L0X.h>
 #include <WiFiNINA.h>
 #include "config.h"
 
+//------ WIFI & MQTT CONFIG ------//
 WiFiClient wifi;
 MqttClient mqtt(wifi);
 
 //WiFi network info: ssid and password
-const char wifi_ssid[] = "Samsung Galaxy S7 6061";
-const char wifi_pass[] = "dnup0849";
+const char wifi_ssid[] = TETHER_WIFI_SSID;
+const char wifi_pass[] = TETHER_WIFI_PASS;
 
 //MQTT broker info: url and port (1883 default for MQTT)
-const char broker[] = "public.cloud.shiftr.io";
-const int  port = 1883;
+const char broker[] = YONE_MQTT_URL;
+const int  port = YONE_MQTT_PORT;
 
 //if needed: broker authentication credentials
-const char mqtt_user[] = "theyonetwork";
-const char mqtt_pass[] = "ConnDevSP24";
-// const char mqtt_user[] = "public";
-// const char mqtt_pass[] = "public";
+const char mqtt_user[] = YONE_MQTT_USER;
+const char mqtt_pass[] = YONE_MQTT_PASS;
 
 //the topic this device will publish messages to
-const char pubTopic[] = "xtina";
+const char pubTopic[] = "Jasper_TOF";
+
+
+//------ TIME OF FLIGHT INIT ------//
+Adafruit_VL53L0X lox = Adafruit_VL53L0X();
+
+//------ HARDWARE ------//
+const int redLedPin = 14;
+const int greenLedPin = 15;
+const int buttonPin = 12;
+
+//------ Global vars ------//
+// char lastMeasure[] = "";
+int lastMeasure = 9999;
+int buttonState = 0;
+const int sendInterval = 1000;
+
 
 void setup() {
+
+  // indicator lights, button
+  pinMode(redLedPin, OUTPUT);
+  pinMode(greenLedPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+
+
+  // serial begin
   Serial.begin(9600);
+
+  // init TOF sensor
+  if (!lox.begin()) {
+    Serial.println(F("Failed to boot VL53L0X"));
+    while(1);
+  }
+
+  //------ WIFI ------//
   Serial.print("Connecting to WiFi");
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     WiFi.begin(wifi_ssid, wifi_pass);
+
+    digitalWrite(greenLedPin, HIGH);  // green indic light while trying to connect to wifi
+
     delay(1000);
   }
+  digitalWrite(greenLedPin, LOW);  // green indic light off once wifi connected
   Serial.println("\nWiFi connected!");
+
+
+  //------ MQTT ------//
+  digitalWrite(redLedPin, HIGH);  // red indic light while trying to connect to mqtt
 
   //give your device any name, to use for identification
   mqtt.setId("Jasper");
@@ -45,17 +86,38 @@ void setup() {
     Serial.print("MQTT connection failed! Error code = ");
     Serial.println(mqtt.connectError());
     while (true) {}; //do nothing forever
-  } else Serial.println("MQTT connected.");
+  } else {
+    Serial.println("MQTT connected.");
+    digitalWrite(redLedPin, LOW);  // red indic light off once mqtt connected
+
+  }
 }
 
-const int sendInterval = 1000;
+
+
 void loop() {
 
+  buttonState = digitalRead(buttonPin);
+
+  VL53L0X_RangingMeasurementData_t measure;
+        
+  lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
+
+  if (measure.RangeStatus != 4) {  // phase failures have incorrect data
+    lastMeasure = measure.RangeMilliMeter;
+  } else {
+    lastMeasure = 9999;
+  }
+
   if (millis() % sendInterval < 10) {
+    Serial.println(lastMeasure);
     mqtt.beginMessage(pubTopic);
-    mqtt.print(millis());
+    // mqtt.print(lastMeasure + ',' + String(buttonState));
+    mqtt.print(lastMeasure);
     mqtt.endMessage();
     delay(10);
   }
-  
+ 
 }
+
+
---
+++ Connected_Devices_SP24/HW4/public/script.js
@@ -0,0 +1,43 @@
+// config data is in config.js -- not pushed to repo
+const broker = YONE_MQTT_URL;
+const mqtt_port = YONE_MQTT_PORT; 
+        
+const mqtt_user = YONE_MQTT_USER;
+const mqtt_pass = YONE_MQTT_PASS;
+
+
+// client ID
+const clientID = "Jasper Client";
+
+// the topic incoming messages should be sent to
+const subTopic = "Jasper_TOF";
+        
+const MQTT = new Paho.MQTT.Client(broker, mqtt_port, clientID);
+
+MQTT.connect({
+    onSuccess: mqttConnected, // callback function for successful connection
+    userName: mqtt_user, // username
+    password: mqtt_pass, // password
+    useSSL: true
+});
+
+//called on successful connection - enables the MQTT message button
+//you should subscribe to topics in here as well
+function mqttConnected() {
+    MQTT.subscribe(subTopic);
+    MQTT.onMessageArrived = handleMQTTMessage; //callback function for incoming messages
+    console.log("MQTT connected successfully.");
+    document.querySelector("#sendMQTTButton").disabled = false;
+}
+
+//send an MQTT message with a defined topic and payload
+function sendMQTTMessage(topic, payload) {
+    const newMessage = new Paho.MQTT.Message(payload);
+    newMessage.destinationName = topic;
+    MQTT.send(newMessage);
+}
+
+function handleMQTTMessage(msg) {
+    document.querySelector("#inTopic").innerHTML = `Topic: ${msg.destinationName}`;
+    document.querySelector("#inPayload").innerHTML = msg.payloadString;
+}
